1314e8029ed5eb8d2eb050d4ec1bc65e5eddc8d9
PabloMickaelQuevedo <drag-rox@hotmail.com>
2022-10-21 00:11:22 -0300

    Criado nova classe Startup.cs

---
+++ AR.Presentation/Startup.cs
@@ -0,0 +1,43 @@
+﻿using AR.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+
+namespace AR.Presentation
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "AR.Presentation", Version = "v1" }); });
+
+            services.AddDbContext<ContextoPrincipal>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("ApiRestConnectionString")));
+        }
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
